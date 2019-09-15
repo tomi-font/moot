093939b907f7bea447fbf254e24f18a3bdff2276
Tomi <>
2019-09-15 13:17:05 +0200

    reading file into buffer

--- src/Factory.cc
+++ src/Factory.cc
@@ -7,17 +7,43 @@
 #include <Component/CPlayer.hh>
 #include <Component/CCollisionBox.hh>
 #include <Component/CRigidbody.hh>
+#include <fstream>
 
 #ifndef RELEASE
 #include <iostream>
 #endif
 
+#define TEMPLATES_DIR "data/templates/"
+
 bool	Factory::loadTemplate(const std::string& name, TemplateMap::iterator* tempIter)
 {
-#ifndef RELEASE
-	std::cerr << "didn't find template for " << name << std::endl;
-	return false;
-#endif
+	char*	content;
+// first reading the whole file into content
+	{
+		std::ifstream	ifs(TEMPLATES_DIR + name, std::ios::ate);
+
+		if (ifs.fail())
+		{
+		#ifndef RELEASE
+			std::cerr << "didn't find template for " << name << std::endl;
+		#endif
+			return false;
+		}
+
+		std::streamsize	len = ifs.tellg();
+		if (len == std::numeric_limits<std::streamsize>::max())
+			return false;
+		content = new char[len + 1];
+		content[len] = '\0';
+		ifs.seekg(0);
+		ifs.read(content, len);
+		ifs.close();
+
+		if (ifs.fail())
+			return false;
+	}
+
+	return true;
 }
 void	Factory::create(const std::string& name, World* world)
 {
