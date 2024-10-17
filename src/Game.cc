7a07fa1d3e410df8af5a816b0e0d925e86600118
Tomi Fontanilles <tomi@tomifont.com>
2024-10-17 19:09:50 +0300

    set max framerate to 60
    For when Vsync setting is not supported.

--- src/Game.cc
+++ src/Game.cc
@@ -8,6 +8,7 @@ Game::Game() : m_world(&m_window)
 
 	m_window.create(halfScreen, {});
 	m_window.setPosition(sf::Vector2i(halfScreen.width / 2, halfScreen.height / 2));
+	m_window.setFramerateLimit(60);
 	m_window.setVerticalSyncEnabled(true);
 	m_window.setKeyRepeatEnabled(false);
 }
